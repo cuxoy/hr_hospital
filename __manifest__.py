87d32acd4ab08631ac7803263e3196a1f6d65af1
Cuxoy <cccccuxoy@gmail.com>
2026-04-27 11:42:12 +0300

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Odoo School Library',
+    'version': '1.0.1.0.0',
+    'author': 'cuxoy',
+    'website': 'https://www.odoo.com',
+    'category': 'Customization',
+    'license': 'OPL-1',
+    'depends': [
+        'base',
+    ],
+    'external_dependencies': {
+        'python': []
+    },
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'images': ['static/description/icon.png'],
+}
